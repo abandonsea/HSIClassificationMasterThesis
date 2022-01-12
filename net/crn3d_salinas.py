209877cedc8690fce24227a0e3365d9acb36cf48
Pedro Vieira <pedrovieira@arrowplan.com>
2022-01-12 11:03:18 +0100

    Implement network architecture for the indian pines and salinas datasets

--- net/crn3d_salinas.py
+++ net/crn3d_salinas.py
@@ -13,29 +13,38 @@ from net.blocks import ResidualBlock
 
 
 class CRN3D(nn.Module):
-    def __init__(self, bands, num_classes):
-        # image_patch: 30x13x13
+    def __init__(self, num_classes):
+        # Patch size: [1, 20, 23, 23]
         super().__init__()
-        self.feature = nn.Sequential(
-            nn.Conv3d(1, 20, (9, 3, 3)),
-            nn.ReLU(inplace=True),
-            nn.MaxPool3d((1, 2, 2)),
+
+        self.relu = nn.ReLU()
+        self.block1 = nn.Sequential(
+            nn.Conv3d(1, 16, (3, 3, 3)),  # Output size: [16, 18, 21, 21]
+            nn.BatchNorm3d(16), self.relu,
+            ResidualBlock(16), self.relu,
+            nn.MaxPool3d((1, 2, 2)),  # Output size: [16, 18, 10, 10]
+            nn.Dropout3d(0.05)
+        )
+        self.block2 = nn.Sequential(
+            nn.Conv3d(16, 32, (3, 3, 3)),  # Output size: [32, 16, 8, 8]
+            nn.BatchNorm3d(32), self.relu,
+            ResidualBlock(32), self.relu,
+            nn.MaxPool3d((1, 2, 2)),  # Output size: [32, 16, 4, 4]
             nn.Dropout3d(0.05),
-            nn.Conv3d(20, 40, (9, 3, 3)),
-            nn.ReLU(inplace=True),
-            nn.MaxPool3d((1, 2, 2))
+            nn.Conv3d(32, 64, (3, 3, 3), padding=(1, 0, 0)),  # Output size: [64, 16, 2, 2]
+            nn.BatchNorm3d(64), self.relu,
+            nn.MaxPool3d((2, 2, 2)),  # Output size: [64, 8, 1, 1]
         )
         self.classifier = nn.Sequential(
-            nn.Dropout(0.05),
-            nn.Linear((bands - 16) * 40, 80),
-            nn.ReLU(inplace=True),
-            nn.Linear(80, num_classes)
+            nn.Linear(512, 128), self.relu,
+            nn.Linear(128, num_classes)
         )
 
     def forward(self, x):
         # Input size: [batch_size, 1, depth, h, w]
-        out = self.feature(x)
-        batch_size = out.shape[0]
-        out = out.view((batch_size, -1))
+        out = self.block1(x)
+        out = self.block2(out)
+
+        out = out.view((-1, 256))
         out = self.classifier(out)
         return out
