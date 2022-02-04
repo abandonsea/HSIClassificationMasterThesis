d3fd778e651680caa2780151f1291320618bad4a
Pedro Vieira <pedrovieira@arrowplan.com>
2022-02-04 12:17:40 +0100

    Complete script implementation and improve figures quality

--- utils/extract_noise_results.py
+++ utils/extract_noise_results.py
@@ -13,13 +13,15 @@ plt.style.use('bmh')
 PATH = '../../../Results/noise/'
 
 DATASETS = ['paviau', 'indian_pines', 'salinas']
-NETWORKS = ['sdmm', 'dffn', 'vscnn', 'sae3ddrn']
+NETWORKS = ['sdmm', 'dffn', 'vscnn', 'sae3ddrn', '3dcrn']
 TEST_CASES = ['full', 'reduced_10', 'reduced_05', 'reduced_01']
 NOISE_TYPE = ['salt_and_pepper', 'additive_gaussian', 'multiplicative_gaussian',
               'section_mul_gaussian', 'single_section_gaussian']
 
 VALUE_POSITION = 3
 TEST_SIZE = 10
+
+section_order = [6, 4, 2, 0, 1, 3, 5, 7]
 
 
 # Get test results from text file
@@ -68,6 +70,13 @@ def get_values(filename):
 
 # Main for running script independently
 def main():
+    data_dict = {'paviau': 'Pavia University', 'indian_pines': 'Indian Pines', 'salinas': 'Salinas'}
+    case_dict = {'full': '80%', 'reduced_10': '10%', 'reduced_05': '5%', 'reduced_01': '1%'}
+    noise_dict = {'salt_and_pepper': 'Amount of affected pixels',
+                  'additive_gaussian': 'Noise variance with respect to the data\'s',
+                  'multiplicative_gaussian': 'Noise variance with respect to the data\'s'}
+    net_dict = {'sdmm': 'S-DMM', 'dffn': 'DFFN', 'vscnn': 'VSCNN', 'sae3ddrn': 'SAE-3DDRN', '3dcrn': '3D-CRN'}
+
     for data in DATASETS:  # Run for 3 datasets
         for case in TEST_CASES:  # Run for 4 test cases
             zero_noise = {}
@@ -84,7 +93,7 @@ def main():
 
                     if noise == 'salt_and_pepper':
                         zero_noise[net] = nodes[net][0]
-                    else:
+                    elif noise == 'additive_gaussian' or noise == 'multiplicative_gaussian':
                         nodes[net].insert(0, zero_noise[net])
 
                 # Plot graphs for the simple noise types
@@ -92,27 +101,53 @@ def main():
                 if noise in simple_noise_types:
                     # Generate graph for the current values
                     fig, ax = plt.subplots()
-                    fig.suptitle(f'Using {noise} noise')
+                    # fig.suptitle(f'Using {noise} noise')
 
                     size = 0
                     for key in nodes:
                         labels, values = zip(*nodes[key])
-                        labels = [f'{str(100 * float(label))}%' for label in labels]
+                        if noise == 'salt_and_pepper':
+                            labels = [f'{str(100 * float(label))}%' for label in labels]
+                        else:
+                            labels = [f'{100 * float(label):.0f}%' for label in labels]
                         size = len(labels)
 
-                        ax.plot(labels, values, linewidth=2.0, label=f'{key} network')
+                        ax.plot(labels, values, linewidth=2.0, label=net_dict[key])
 
                     ax.set(xlim=(0, size-1), xticks=np.arange(0, size),
                            ylim=(0.0, 1.1), yticks=np.arange(0.2, 1.1, 0.2))
-                    ax.set(xlabel='Amount of noise', ylabel='Average accuracy',
-                           title=f'Subplot title')
+                    ax.set(xlabel=noise_dict[noise], ylabel='Average accuracy',
+                           title=f'Train split: {case_dict[case]}')
                     ax.legend()
                     # plt.show()
                     plt.savefig(f'{PATH}{data}_{case}_{noise}.png')
 
-                # TODO: Plot graphs for the section noise types
                 else:
-                    a = 2
+                    # Generate graph for the current values
+                    fig, ax = plt.subplots()
+                    # fig.suptitle(f'Using {noise} noise')
+
+                    size = 0
+                    for key in nodes:
+                        labels, values = zip(*nodes[key])
+                        labels = [f'S{label}' for label in labels]
+                        size = len(labels)
+
+                        aux_labels, aux_values = [], []
+                        for i in range(size):
+                            aux_labels.append(labels[section_order[i]])
+                            aux_values.append(values[section_order[i]])
+                        labels, values = aux_labels, aux_values
+
+                        ax.plot(labels, values, linewidth=2.0, label=net_dict[key])
+
+                    ax.set(xlim=(0, size - 1), xticks=np.arange(0, size),
+                           ylim=(0.0, 1.1), yticks=np.arange(0.2, 1.1, 0.2))
+                    ax.set(xlabel='Section', ylabel='Average accuracy',
+                           title=f'Train split: {case_dict[case]}')
+                    ax.legend()
+                    # plt.show()
+                    plt.savefig(f'{PATH}{data}_{case}_{noise}.png')
 
 
 if __name__ == '__main__':
